7fbec7e70a07752a2682b345f68c1d0699dc60b8
jerry <2380347175@qq.com>
2023-10-31 17:13:53 +0800

    陈杰林-第一次提交

---
+++ test1.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<String.h>
+void encrypt()
+{
+	int len,i;
+	char temp1,password[20];
+	printf("请输入密码：");
+	scanf("%s",password);
+	len = strlen(password);
+	for(i = 0;i<len;i++)
+	{
+		password[i] = password[i]+i+1+3; 
+	}
+	temp1 = password[0];
+	password[0] = password[len-1];
+	password[len-1] = temp1;
+	for(i = 0;i<len/2;i++)
+	{
+		char temp2;
+		temp2 = password[i];
+		password[i] = password[len-1-i];
+		password[len-1-i] = temp2;
+	}
+	printf("加密后的密码为：%s\n",password);
+}
+void decrypt()
+{
+	int len,i;
+	char temp4,password[20];
+    printf("请输入密码：");
+	scanf("%s",password);
+	len = strlen(password);
+	for(i = 0;i<len/2;i++)
+	{
+		char temp3;
+		temp3 = password[i];
+		password[i] = password[len-1-i];
+		password[len-1-i] = temp3;
+	}
+	temp4 = password[0];
+	password[0] = password[len-1];
+	password[len-1] = temp4;
+	for(i = 0;i<len;i++)
+	{
+		password[i] = password[i]-1-i-3;
+	}
+	printf("解密后的密码为：%s\n",password);
+}
+void menu()
+{
+	printf("----------------------\n");
+    printf("----------------------\n");
+	printf(" 欢迎使用密码管理系统\n");
+	printf("----------------------\n");
+	printf("----------------------\n");
+    printf("请选择操作：\n");
+	printf("1.加密\n");
+	printf("2.解密\n");
+	printf("3.退出\n");
+}
+int main()
+{
+	int flag = 1;
+	while(flag)
+	{
+		int n = 0;
+		menu();
+		printf("请输入选择序号：");
+		scanf("%d",&n);
+		switch(n)
+		{
+			case 1:
+	            encrypt();
+				break;
+			case 2:
+	            decrypt();
+				break;
+			case 3:
+				flag = 0;
+				break;
+			default:
+				break;
+		}
+	}
+    return 0;
+}
