0990443db17d747189c6df0d4261356f515cdff5
jerry <2380347175@qq.com>
2023-11-13 22:43:23 +0800

    陈杰林-第三次提交

--- test1.c
+++ test1.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<String.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<time.h>
 //加密函数
 void encrypt()
 {
@@ -26,6 +29,7 @@ void encrypt()
 		password[len-1-i] = temp2;
 	}
 	printf("加密后的密码为：%s\n",password);
+	printf("\n");
 }
 //解密函数
 void decrypt()
@@ -50,18 +54,102 @@ void decrypt()
 		password[i] = password[i]-1-i-3;
 	}
 	printf("解密后的密码为：%s\n",password);
+	printf("\n");
+}
+void strength()
+{
+	char password[20];
+	int i,length,sum,n1 = 0,n2 = 0,n3 = 0;
+	printf("请输入密码：");
+	getchar();
+	gets(password);
+	length = strlen(password);
+	if(length<8)
+	{
+		sum = 1;
+	}
+	if(length>=8)
+	{
+		for(i = 0;i<length;i++)
+		{
+			if(isdigit(password[i]))
+			{
+				n1 = 1;
+			}
+			else if(isupper(password[i]))
+			{
+				n2 = 1;
+			}
+			else if(islower(password[i]))
+			{
+				n3 = 1;
+			}
+		}
+		sum = n1+n2+n3;
+	}
+	switch(sum)
+	{
+		case 1:
+			printf("密码强度：弱\n");
+			break;
+		case 2:
+			printf("密码强度：中\n");
+			break;
+		case 3:
+			printf("密码强度：强\n");
+			break;
+		default:
+			printf("请重新输入密码！\n");
+			break;
+	}
+	printf("\n");
+}
+void generate()
+{
+	char password[20];
+	int i,len,mark;
+	int n1 = 0,n2 = 0,n3 = 0;
+	srand("请输入密码长度：");
+	scanf("%d",&len);
+	do
+	{
+		for(i = 0;i<len;i++)
+		{
+			mark = rand()%3+1;
+			switch(mark)
+			{
+				case 1:
+					password[i] = rand()%10+'0';
+					n1 = 1;
+					break;
+				case 2:
+					password[i] = rand()%26+'A';
+					n2 = 1;
+					break;
+				case 3:
+					password[i] = rand()%26+'a';
+					n3 = 1;
+					break;
+			}
+		}
+		password[len] = '\0';
+	}while(n1+n2+n3!=3);
+	printf("生成的密码为：%s\n",password);
+	printf("\n");
 }
 void menu()
 {
 	printf("----------------------\n");
-    printf("----------------------\n");
+        printf("----------------------\n");
 	printf(" 欢迎使用密码管理系统\n");
 	printf("----------------------\n");
 	printf("----------------------\n");
-    printf("请选择操作：\n");
+        printf("请选择操作：\n");
 	printf("1.加密\n");
 	printf("2.解密\n");
-	printf("3.退出\n");
+	printf("3.判断密码强度\n");
+	printf("4.生成密码\n");
+	printf("5.退出\n");
 }
 int main()
 {
@@ -75,12 +163,18 @@ int main()
 		switch(n)
 		{
 			case 1:
-	            encrypt();
+	                        encrypt();
 				break;
 			case 2:
-	            decrypt();
+	                        decrypt();
 				break;
 			case 3:
+				strength();
+				break;
+			case 4:
+				generate();
+				break;
+			case 5:
 				flag = 0;
 				break;
 			default:
